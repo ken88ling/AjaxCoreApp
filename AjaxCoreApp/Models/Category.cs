f6d1c0bdd5b204606da01afd6c3d719ddb786fd8
Ken Ling <ken88ling@gmail.com>
2017-12-27 11:17:51 +0800

    Regenerate CRUD

--- AjaxCoreApp/Models/Category.cs
+++ AjaxCoreApp/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,11 @@ namespace AjaxCoreApp.Models
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
 
+        public ICollection<Product> Products { get; set; }
+
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
     }
 }
